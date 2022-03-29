342c90355c0ada323992c866d2a9ffb01167ff26
Edgar Marcos <edgaricloud@gmail.com>
2022-03-29 10:49:20 +0100

    Creaction of curd Class
    Prepared Statements

---
+++ class/ClassCrud.php
@@ -0,0 +1,26 @@
+<?php
+include("ClassConexao.php");
+
+#Prepared Statement
+class ClassCrud extends ClassConexao{
+
+    private $Crud;
+    private $Contador;
+
+    #preparacao das declarativas
+    private function PreparedStatements($Query,$Parametros){
+        $this->CountParametros($Parametros);
+        $this->Crud=$this->conectaBD()->prepare($Query);
+        if($this->Contador > 0){
+            for ($i=1; $i <=$this->Contador; $i++) { 
+                $this->Crud->bindValue($i,$Parametros[$i-1]);
+            }
+        }
+    }
+    #Contador de Parametros
+    private function CountParametros($Parametros){
+        $this->Contador=count($Parametros);
+    }
+}
+
+?>
