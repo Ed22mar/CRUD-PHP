43a049f84e5baccc1d0b737e55f3ad529f830c4f
Edgar Marcos <edgaricloud@gmail.com>
2022-04-02 20:56:36 +0100

    Update Index

--- index.php
+++ index.php
@@ -1,15 +1,10 @@
-<?php  
-    include("class/ClassConexao.php")
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php include('includes/header.php');?>
+    <div class="content">
+        <?php  
+            //include_once("class/ClassConexao.php");
+            include("class/ClassCrud.php");
+        ?>
+    </div>
+<?php include('includes/footer.php');?>
+
+
